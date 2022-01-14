6173ab580d1466093700dc1a84367c117517fce0
gaurav-de <69707934+gaurav-de@users.noreply.github.com>
2022-01-14 19:44:55 +0530

    Create shuffler.cs

---
+++ shuffler.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace shuffling_colsol_application
+{
+    internal class shuffler
+    {
+        public static String string_shuffler(String need_to_shouffled)
+        { 
+            Char temp_one;
+            List<string> list = new List<string>();
+            int nameLength = need_to_shouffled.Length;
+
+            while(nameLength > 0)
+            {
+                temp_one = need_to_shouffled[(nameLength - 1)];
+                list.Add(temp_one.ToString());
+                nameLength--;
+            }
+
+            var shuffledcards = list.OrderBy(a => Guid.NewGuid()).ToList();
+            string str = String.Join("", shuffledcards);
+            return str;
+        }
+
+        static void Main(String[] args)
+        {
+            Console.WriteLine(string_shuffler("hello world!"));
+        }
+    }
+}
